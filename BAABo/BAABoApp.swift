4e80604d0b27940c7b911e4965c2c12b930a53a4
chami3i <hongcm2002@naver.com>
2025-08-04 16:17:06 +0900

    Initial Commit

---
+++ BAABo/BAABoApp.swift
@@ -0,0 +1,17 @@
+//
+//  BAABoApp.swift
+//  BAABo
+//
+//  Created by chaem on 8/4/25.
+//
+
+import SwiftUI
+
+@main
+struct BAABoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
