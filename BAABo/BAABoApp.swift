a59cb939bbcbcdedf34bb54e302ff1cee8ccd9d7
Lee Sion <sion8120@gmail.com>
2025-08-12 01:10:27 +0900

    Add Auth Anonymously

--- BAABo/BAABoApp.swift
+++ BAABo/BAABoApp.swift
@@ -6,12 +6,22 @@
 //
 
 import SwiftUI
+import FirebaseCore
+import FirebaseAuth
 
 @main
 struct BAABoApp: App {
+    init() {
+        print("🔥 Firebase 초기화 시도")
+        FirebaseApp.configure()
+        AuthService.signInAnonymouslyIfNeeded() // 앱 실행 시 익명 로그인
+    }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
     }
+    
+
 }
