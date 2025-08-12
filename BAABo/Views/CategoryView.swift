878b5c175d3a271780f03f51e1498b5a188dbe9c
Jelly <angelaym@naver.com>
2025-08-12 17:16:20 +0900

    0812 CategoryView 팝업 추가

--- BAABo/Views/CategoryView.swift
+++ BAABo/Views/CategoryView.swift
@@ -32,7 +32,12 @@ struct CategoryView: View {
     @State private var timerEnded: Bool = false     // 타이머 상태 변수 설정(끝났다면 화면 바꾸기)
     @State private var selectedCategories: [Category] = []      // 사용자가 선택한 카테고리 저장
     @State private var isAllLikedSelected: Bool = false     // 다 좋아 버튼 눌렸는지 여부
-    @State private var navigateToResult: Bool = false       // CategoryResultView로 전환
+//    @State private var navigateToResult: Bool = false       // CategoryResultView로 전환
+    @State private var showResultPopup: Bool = false        // 결과 팝업 설정(결과 보기 버튼을 눌렀을 경우)
+    @State private var moveToPlaceView = false              // PlaceView로 전환
+    
+    
+    let selectedCategory = "아시안" // TODO: 나중에 동적으로 바꾸기
     
     let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
     
@@ -41,142 +46,198 @@ struct CategoryView: View {
     
     var body: some View {
         NavigationStack {
-            VStack {
-                // MARK: 상단바
-                HStack {
-                    Text("카테고리 선택")     // 페이지 이름
+            ZStack {
+                VStack {
+                    // MARK: 상단바
+                    HStack {
+                        Text("카테고리 선택")     // 페이지 이름
                         //.font(.system(size: 30, weight: .bold))
                         //.bold()
-                        .font(.title)
-                        .fontWeight(.bold)
-                    Spacer()
-                    ZStack {    // 타이머
-                        Circle()
-                            .fill(timerEnded ? Color("4_silverColor") : Color(.accent))  // 타이머 활성화 여부에 따라 버튼 색 변경
-                            .frame(width:35, height:35)
-                        Image(systemName: "timer")
-                            .resizable()
-                            .scaledToFit()
-                            .foregroundColor(.white)
-                            .frame(width:20, height:20)
+                            .font(.title)
+                            .fontWeight(.bold)
+                        Spacer()
+                        ZStack {    // 타이머
+                            Circle()
+                                .fill(timerEnded ? Color("4_silverColor") : Color(.accent))  // 타이머 활성화 여부에 따라 버튼 색 변경
+                                .frame(width:35, height:35)
+                            Image(systemName: "timer")
+                                .resizable()
+                                .scaledToFit()
+                                .foregroundColor(.white)
+                                .frame(width:20, height:20)
+                        }
+                        Text(String(format:"00:%02d", timeRemaining))   // 남은 시간
+                            .font(.system(size: 27, weight: .semibold))
+                            .foregroundColor(timerEnded ? Color("4_silverColor") : Color(.accent))
                     }
-                    Text(String(format:"00:%02d", timeRemaining))   // 남은 시간
-                        .font(.system(size: 27, weight: .semibold))
-                        .foregroundColor(timerEnded ? Color("4_silverColor") : Color(.accent))
-                }
-                .padding(.horizontal)
-                .padding(.bottom, 30)   // 상단바와 선택버튼 사이 간격 조정
-                
-                // MARK: 카테고리 선택 버튼 (정렬)
-                LazyVGrid(columns: columns, spacing:17) {
-                    ForEach(sampleCategories) {category in
-                        let isSelected = selectedCategories.contains(where: { $0.name == category.name })
-                        let selectionIndex = selectedCategories.firstIndex(where: { $0.name == category.name })
+                    .padding(.horizontal)
+                    .padding(.bottom, 30)   // 상단바와 선택버튼 사이 간격 조정
+                    
+                    // MARK: 카테고리 선택 버튼 (정렬)
+                    LazyVGrid(columns: columns, spacing:17) {
+                        ForEach(sampleCategories) {category in
+                            let isSelected = selectedCategories.contains(where: { $0.name == category.name })
+                            let selectionIndex = selectedCategories.firstIndex(where: { $0.name == category.name })
+                            
+                            CategoryBox(     // 카테고리 이름 순서대로 붙여줌
+                                category :category,
+                                isSelected: selectedCategories.contains(where: {$0.name == category.name}),
+                                isAllLikedSelected: isAllLikedSelected,
+                                selectionIndex: selectionIndex,
+                                timerEnded: timerEnded,
+                                onTap: {
+                                    handleCategoryTap(category)
+                                }
+                            )
+                        }
+                    }
+                    .padding(.horizontal, 16)
+                    .padding(.bottom, 16)
+                    
+                    
+                    // MARK: 다좋아 버튼
+                    Button(action:{
+                        // 버튼 기능
+                        isAllLikedSelected.toggle()     // 버튼 선택과 해제를 자유롭게 할 수 있도록 토글 사용
                         
-                        CategoryBox(     // 카테고리 이름 순서대로 붙여줌
-                            category :category,
-                            isSelected: selectedCategories.contains(where: {$0.name == category.name}),
-                            isAllLikedSelected: isAllLikedSelected,
-                            selectionIndex: selectionIndex,
-                            timerEnded: timerEnded,
-                            onTap: {
-                                handleCategoryTap(category)
-                            }
+                        if isAllLikedSelected {
+                            selectedCategories = []     // 다좋아 버튼 활성화되면 이전 선택들은 초기화
+                        }
+                    }) {
+                        HStack {
+                            Image("배려")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 67, height: 67)
+                            Text("다~좋아!")
+                            //.font(.system(size: 32))
+                                .font(.title)
+                            
+                        }
+                        .foregroundColor(.black)
+                        .padding()
+                        .frame(width: 337, height: 101)
+                        .background(timerEnded ? Color("1_grayColor") : Color(.accent).opacity(0.2))    // 타이머 완료되면 버튼 색 바꾸기
+                        .cornerRadius(20)
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 20)
+                                .stroke(isAllLikedSelected ? Color("6_pinkColor") : Color.clear, lineWidth: 7)
                         )
                     }
-                }
-                .padding(.horizontal, 16)
-                .padding(.bottom, 16)
-                
-                
-                // MARK: 다좋아 버튼
-                Button(action:{
-                    // 버튼 기능
-                    isAllLikedSelected.toggle()     // 버튼 선택과 해제를 자유롭게 할 수 있도록 토글 사용
+                    .disabled(timerEnded)       // 타이머 끝나면 버튼 비활성화
                     
-                    if isAllLikedSelected {
-                        selectedCategories = []     // 다좋아 버튼 활성화되면 이전 선택들은 초기화
+                    .onReceive(timer) { _ in    // 타이머 기능 구현
+                        if timeRemaining > 0 {
+                            timeRemaining -= 1
+                            if timeRemaining == 0 {
+                                timerEnded = true  // 시간 끝났으면 타이머 상태 변수 변경
+                            }
+                        }
                     }
-                }) {
-                    HStack {
-                        Image("배려")
-                            .resizable()
-                            .scaledToFit()
-                            .frame(width: 67, height: 67)
-                        Text("다~좋아!")
-                            //.font(.system(size: 32))
-                            .font(.title)
-                            
+                    
+                    .padding(.bottom, 80)
+                    
+                    // MARK: 결과 보기 버튼
+                    Button(action:{
+                        //                    navigateToResult = true
+                        showResultPopup = true
+                    }) {
+                        HStack {
+                            Text(timerEnded ? "결과 보기" : String(format: "00:%02d 후 결과 보기", timeRemaining))   // 버튼에서 남은 시간 텍스트로 보여주기
+                            //  .font(.system(size: 30, weight: .bold))
+                                .font(.title)
+                                .bold()
+                            Image(systemName: "arrow.right.circle")
+                                .resizable()
+                                .scaledToFit()
+                                .bold()
+                                .frame(width:30, height:30)
+                        }
+                        .foregroundColor(.black)
+                        .frame(width: 337, height: 101)
+                        .background(timerEnded ? Color(.accent) : Color("1_grayColor"))
+                        .cornerRadius(20)
+                        
                     }
-                    .foregroundColor(.black)
-                    .padding()
-                    .frame(width: 337, height: 101)
-                    .background(timerEnded ? Color("1_grayColor") : Color(.accent).opacity(0.2))    // 타이머 완료되면 버튼 색 바꾸기
-                    .cornerRadius(20)
-                    .overlay(
-                        RoundedRectangle(cornerRadius: 20)
-                            .stroke(isAllLikedSelected ? Color("6_pinkColor") : Color.clear, lineWidth: 7)
-                    )
+                    .disabled(!timerEnded)  // 타이머 끝나야 버튼 활성화
                 }
-                .disabled(timerEnded)       // 타이머 끝나면 버튼 비활성화
+                .padding(.horizontal, 20)   // 전체 화면에서 좌우 여백 조정
+                //            .navigationBarBackButtonHidden(true)
+                //            .navigationDestination(isPresented: $navigateToResult) {
+                //                CategoryResultView()
                 
-                .onReceive(timer) { _ in    // 타이머 기능 구현
-                    if timeRemaining > 0 {
-                        timeRemaining -= 1
-                        if timeRemaining == 0 {
-                            timerEnded = true  // 시간 끝났으면 타이머 상태 변수 변경
+                // MARK: 결과 팝업(CategoryResultView)
+                if showResultPopup {
+                    ZStack {
+                        Color.black.opacity(0.4)        // 전체 화면 어둡게
+                            .ignoresSafeArea()
+                            .onTapGesture {
+                                showResultPopup = false
+                            }
+                        VStack {                        // 팝업 설정
+                            Text("오늘의 메뉴")
+                                .font(.system(size: 32, weight: .bold))
+                                .foregroundColor(.accent)
+                            Image("아시안")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 131, height: 131)
+                            Text("아시안")
+                                .font(.system(size: 32, weight: .bold))
+                                .foregroundColor(.accent)
+                            
+                            // 식당 고르러 가기 버튼
+                            Button(action:{
+                                moveToPlaceView = true
+                            }) {
+                                HStack {
+                                    Text("식당 고르러 가기")
+                                        .font(.system(size: 20, weight: .bold))
+                                    Image(systemName: "arrow.right.circle")
+                                        .resizable()
+                                        .scaledToFit()
+                                        .bold()
+                                        .frame(width: 20, height: 20)
+                                }
+                                .foregroundColor(.white)
+                                .padding()
+                                .frame(width: 239, height: 58)
+                                .background(Color.orange)
+                                .cornerRadius(20)
+                            }
+                        }
+                        .frame(width: 274, height: 365)
+                        .background(
+                            RoundedRectangle(cornerRadius: 20)
+                                .fill(
+                                    LinearGradient(colors: [Color("7_gradientOrange"), Color("8_gradientYellow")], startPoint: .topLeading, endPoint: .bottomTrailing)
+                                )
+                        )
+                        .navigationBarBackButtonHidden(true)
+                        .navigationDestination(isPresented: $moveToPlaceView) {
+                            PlaceView(category: selectedCategory)
                         }
                     }
                 }
-                
-                .padding(.bottom, 80)
-                
-                // MARK: 결과 보기 버튼
-                Button(action:{
-                    navigateToResult = true
-                }) {
-                    HStack {
-                        Text(timerEnded ? "결과 보기" : String(format: "00:%02d 후 결과 보기", timeRemaining))   // 버튼에서 남은 시간 텍스트로 보여주기
-                          //  .font(.system(size: 30, weight: .bold))
-                            .font(.title)
-                            .bold()
-                        Image(systemName: "arrow.right.circle")
-                            .resizable()
-                            .scaledToFit()
-                            .bold()
-                            .frame(width:30, height:30)
-                    }
-                    .foregroundColor(.black)
-                    .frame(width: 337, height: 101)
-                    .background(timerEnded ? Color(.accent) : Color("1_grayColor"))
-                    .cornerRadius(20)
-                    
+            }
+        }
+    }
+            // MARK: 카테고리 선택 처리 함수
+            func handleCategoryTap(_ category: Category) {
+                // 다 좋아 버튼 선택 상태에서는 카테고리 선택 불가
+                if isAllLikedSelected {
+                    isAllLikedSelected = false
                 }
-                .disabled(!timerEnded)  // 타이머 끝나야 버튼 활성화
-            }
-            .padding(.horizontal, 20)   // 전체 화면에서 좌우 여백 조정
-            .navigationBarBackButtonHidden(true)
-            .navigationDestination(isPresented: $navigateToResult) {
-                CategoryResultView()
-            }
-        }
-    }
-    // MARK: 카테고리 선택 처리 함수
-    func handleCategoryTap(_ category: Category) {
-        // 다 좋아 버튼 선택 상태에서는 카테고리 선택 불가
-        if isAllLikedSelected {
-                isAllLikedSelected = false
-            }
-        
-        if selectedCategories.contains(where: { $0.name == category.name }) {
-            selectedCategories.removeAll{ $0.name == category.name }
-        } else {
-            if selectedCategories.count < 3 {
-                selectedCategories.append(category)
+                
+                if selectedCategories.contains(where: { $0.name == category.name }) {
+                    selectedCategories.removeAll{ $0.name == category.name }
+                } else {
+                    if selectedCategories.count < 3 {
+                        selectedCategories.append(category)
+                    }
+                }
             }
         }
-    }
-}
    
 // MARK: CategoryBox
 struct CategoryBox: View {      // 카테고리 선택버튼 (세부설정)
